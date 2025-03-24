a0508fdbf7ec3c8fb3ce531d4238cb4d1cd1adc1
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-03-24 16:22:20 +0200

    move tests out of /src

--- src/tests.rs
+++ src/tests.rs
@@ -1,34 +1,40 @@
 use crate::compile::compile_test;
 
+macro_rules! include_test {
+    ($name:literal) => {
+        include_str!(concat!("../tests/", $name, ".pebble"))
+    };
+}
+
 #[test]
 fn inference() {
-    compile_test(include_str!("tests/inference.pebble")).unwrap();
+    compile_test(include_test!("inference")).unwrap();
 }
 
 #[test]
 fn never() {
-    compile_test(include_str!("tests/never.pebble")).unwrap();
+    compile_test(include_test!("never")).unwrap();
 }
 
 #[test]
 fn arrays() {
-    compile_test(include_str!("tests/arrays.pebble")).unwrap();
+    compile_test(include_test!("arrays")).unwrap();
 }
 
 #[test]
 #[should_panic = "expected `!`, found `int`"]
 fn fail_never() {
-    compile_test(include_str!("tests/fail_never.pebble")).unwrap();
+    compile_test(include_test!("fail_never")).unwrap();
 }
 
 #[test]
 #[should_panic = "expected `int`, found `str`"]
 fn fail_variables() {
-    compile_test(include_str!("tests/fail_variables.pebble")).unwrap();
+    compile_test(include_test!("fail_variables")).unwrap();
 }
 
 #[test]
 #[should_panic = "expected `int`, found `str`"]
 fn fail_return() {
-    compile_test(include_str!("tests/fail_return.pebble")).unwrap();
+    compile_test(include_test!("fail_return")).unwrap();
 }
