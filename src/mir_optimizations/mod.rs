53f52a42f5274b36a5de89d702162b10f2abfd62
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-04-01 18:31:05 +0200

    rename compress_blocks to remove_dead_blocks

--- src/mir_optimizations/mod.rs
+++ src/mir_optimizations/mod.rs
@@ -1,9 +1,9 @@
 use crate::mir::{BodyId, Mir};
 
-mod compress_blocks;
 mod not_branch;
 mod redundant_blocks;
 mod redundant_branch;
+mod remove_dead_blocks;
 
 pub fn optimize(mir: &mut Mir) {
     for body in 0..mir.bodies.len() {
@@ -15,5 +15,5 @@ pub fn optimize_body(mir: &mut Mir, body: BodyId) {
     not_branch::optimize(mir, body);
     redundant_branch::optimize(mir, body);
     redundant_blocks::optimize(mir, body);
-    compress_blocks::optimize(mir, body);
+    remove_dead_blocks::optimize(mir, body);
 }
