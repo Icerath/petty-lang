db420e35b55cddaf58699f49fa84c9ba0a6da0f1
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-04-10 16:41:38 +0200

    improve mir display

--- src/mir/display.rs
+++ src/mir/display.rs
@@ -4,8 +4,8 @@ use super::{Constant, Mir, Operand, Place, Projection, RValue, Statement, Termin
 
 impl fmt::Display for Mir {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for (id, body) in self.bodies.iter_enumerated().skip(1 + self.num_intrinsics) {
-            writeln!(f, "fn{id:?}() {{")?;
+        for body in self.bodies.iter().skip(1 + self.num_intrinsics) {
+            writeln!(f, "fn {}() {{", body.name)?;
             for (id, block) in body.blocks.iter_enumerated() {
                 writeln!(f, "{}bb{id:?}: {{", Indent(1))?;
                 for statement in &block.statements {
@@ -15,22 +15,33 @@ impl fmt::Display for Mir {
                             write!(f, "{place} = ")?;
                             match rvalue {
                                 RValue::BinaryExpr { lhs, op, rhs } => {
-                                    write!(f, "{op:?}({lhs}, {rhs})")
+                                    write!(
+                                        f,
+                                        "{op:?}({}, {})",
+                                        lhs.display(self),
+                                        rhs.display(self)
+                                    )
                                 }
-                                RValue::Use(arg) => write!(f, "{arg}"),
+                                RValue::Use(arg) => write!(f, "{}", arg.display(self)),
                                 RValue::Call { function, args } => {
-                                    write!(f, "call {function}")?;
+                                    write!(f, "call {}", function.display(self))?;
                                     write!(f, "(")?;
                                     for (i, arg) in args.iter().enumerate() {
-                                        write!(f, "{}{arg}", if i == 0 { "" } else { ", " })?;
+                                        let sep = if i == 0 { "" } else { ", " };
+                                        write!(f, "{sep}{}", arg.display(self))?;
                                     }
                                     write!(f, ")")
                                 }
                                 RValue::Extend { array, value, repeat } => {
-                                    write!(f, "extend _{array:?}[{value}; {repeat}]")
+                                    write!(
+                                        f,
+                                        "extend _{array:?}[{}; {}]",
+                                        value.display(self),
+                                        repeat.display(self)
+                                    )
                                 }
                                 RValue::UnaryExpr { op, operand } => {
-                                    write!(f, "{op:?}({operand})")
+                                    write!(f, "{op:?}({})", operand.display(self))
                                 }
                             }?;
                         }
@@ -42,9 +53,13 @@ impl fmt::Display for Mir {
                     Terminator::Abort => write!(f, "abort"),
                     Terminator::Goto(to) => write!(f, "goto bb{to:?}"),
                     Terminator::Branch { condition, fals, tru } => {
-                        write!(f, "branch {condition}[false: bb{fals:?}, true: bb{tru:?}]")
+                        write!(
+                            f,
+                            "branch {}[false: bb{fals:?}, true: bb{tru:?}]",
+                            condition.display(self)
+                        )
                     }
-                    Terminator::Return(arg) => write!(f, "return {arg}"),
+                    Terminator::Return(arg) => write!(f, "return {}", arg.display(self)),
                 }?;
                 writeln!(f, "\n{}}}", Indent(1))?;
             }
@@ -65,14 +80,23 @@ impl fmt::Display for Indent {
     }
 }
 
-impl fmt::Display for Operand {
+struct OperandDisplay<'a, 'b>(&'a Mir, &'b Operand);
+struct ConstDisplay<'a, 'b>(&'a Mir, &'b Constant);
+
+impl fmt::Display for OperandDisplay<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Ref(place) => write!(f, "&{place}"),
-            Self::Place(place) => write!(f, "{place}"),
-            Self::Constant(constant) => write!(f, "{constant}"),
-            Self::Unreachable => write!(f, "unreachable"),
+        match self.1 {
+            Operand::Ref(place) => write!(f, "&{place}"),
+            Operand::Place(place) => write!(f, "{place}"),
+            Operand::Constant(constant) => write!(f, "{}", constant.display(self.0)),
+            Operand::Unreachable => write!(f, "unreachable"),
         }
+    }
+}
+
+impl Operand {
+    pub fn display(&self, mir: &Mir) -> impl fmt::Display {
+        OperandDisplay(mir, self)
     }
 }
 
@@ -93,20 +117,25 @@ impl fmt::Display for Place {
     }
 }
 
-impl fmt::Display for Constant {
+impl fmt::Display for ConstDisplay<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if !matches!(self, Self::Func(..)) {
+        if !matches!(self.1, Constant::Func(..)) {
             write!(f, "const ")?;
         }
-        match self {
-            Self::UninitStruct { size } => write!(f, "struct {{ {size:?} }}"),
-            Self::EmptyArray => write!(f, "[]"),
-            Self::Unit => write!(f, "()"),
-            Self::Bool(bool) => write!(f, "{bool}"),
-            Self::Int(int) => write!(f, "{int}"),
-            Self::Char(char) => write!(f, "{char:?}"),
-            Self::Str(str) => write!(f, "{str:?}"),
-            Self::Func(id) => write!(f, "fn{id:?}"),
+        match self.1 {
+            Constant::UninitStruct { size } => write!(f, "struct {{ {size:?} }}"),
+            Constant::EmptyArray => write!(f, "[]"),
+            Constant::Unit => write!(f, "()"),
+            Constant::Bool(bool) => write!(f, "{bool}"),
+            Constant::Int(int) => write!(f, "{int}"),
+            Constant::Char(char) => write!(f, "{char:?}"),
+            Constant::Str(str) => write!(f, "{str:?}"),
+            Constant::Func(id) => write!(f, "{}", self.0.bodies[*id].name),
         }
+    }
+}
+impl Constant {
+    pub fn display(&self, mir: &Mir) -> impl fmt::Display {
+        ConstDisplay(mir, self)
     }
 }
