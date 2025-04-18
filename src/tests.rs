0cf4446889a52308671da7a9ad37e9de766d62b7
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-04-18 02:43:15 +0200

    nit tests

--- src/tests.rs
+++ src/tests.rs
@@ -14,25 +14,31 @@ macro_rules! test {
             compile_test(concat!("tests/", stringify!($name), ".pebble")).unwrap();
         }
     };
+    {$($name:ident)*  $($fails:literal $fail_name: ident)*} => {
+        $(test!($name);)*
+        $(test!($fails $fail_name);)*
+    };
 }
 
-test! { inference }
-test! { never }
-test! { arrays }
-test! { generics }
-test! { if_expr }
-test! { functions }
-test! { structs }
-test! { ref_assignment }
-test! { precedence }
-test! { chars }
-test! { strings }
-test! { format }
-test! { recursion }
-test! { refs }
-test! { variables }
-
-test! { "expected `!`, found `int`" fail_never }
-test! { "expected `int`, found `str`" fail_variables }
-test! { "expected `int`, found `str`" fail_return }
-test! { "assertion failed" fail_assert }
+test! {
+    inference
+    never
+    arrays
+    generics
+    if_expr
+    functions
+    structs
+    ref_assignment
+    precedence
+    chars
+    strings
+    format
+    recursion
+    refs
+    variables
+    // should panic
+    "expected `!`, found `int`" fail_never
+    "expected `int`, found `str`" fail_variables
+    "expected `int`, found `str`" fail_return
+    "assertion failed" fail_assert
+}
