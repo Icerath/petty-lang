2fc025948a005d2e93e163335c6f090e2510a201
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-04-22 11:07:47 +0200

    move looping logic to it's own file

---
+++ src/hir_lowering/loops.rs
@@ -0,0 +1,99 @@
+use std::mem;
+
+use super::{
+    BinaryOp, BlockId, Constant, ExprId, Local, Lowering, Operand, RValue, Symbol, Terminator,
+    UnaryOp,
+};
+
+impl Lowering<'_, '_, '_> {
+    pub fn range_for(&mut self, ident: Symbol, iter: ExprId, body: &[ExprId]) {
+        let range = self.lower(iter);
+        let lo =
+            self.assign_new(RValue::UnaryExpr { op: UnaryOp::RangeStart, operand: range.clone() });
+        let hi = self.assign_new(RValue::UnaryExpr { op: UnaryOp::RangeEnd, operand: range });
+
+        self.for_loop(
+            ident,
+            body,
+            |lower| {
+                lower.assign_new(RValue::BinaryExpr {
+                    lhs: Operand::local(lo),
+                    op: BinaryOp::IntLess,
+                    rhs: Operand::local(hi),
+                })
+            },
+            |lower| {
+                let ident_var = lower.assign_new(Operand::local(lo));
+                lower.assign(
+                    lo,
+                    RValue::BinaryExpr {
+                        lhs: Operand::local(lo),
+                        op: BinaryOp::IntAdd,
+                        rhs: Constant::Int(1).into(),
+                    },
+                );
+                ident_var
+            },
+        );
+    }
+
+    pub fn for_loop(
+        &mut self,
+        ident: Symbol,
+        body: &[ExprId],
+        condition: impl FnOnce(&mut Self) -> Local,
+        iter: impl FnOnce(&mut Self) -> Local,
+    ) {
+        self.lower_loop(
+            body,
+            |lower| Some(condition(lower)),
+            |lower| {
+                let ident_var = iter(lower);
+                lower.current_mut().scope().variables.insert(ident, ident_var);
+            },
+        );
+    }
+
+    pub fn lower_loop(
+        &mut self,
+        body: &[ExprId],
+        condition: impl FnOnce(&mut Self) -> Option<Local>,
+        iter: impl FnOnce(&mut Self),
+    ) {
+        self.finish_next();
+        let condition_block = self.current_block();
+
+        let prev_loop = mem::take(&mut self.current_mut().breaks);
+        self.current_mut().breaks.push(condition_block);
+        let prev_continue = self.current_mut().continue_block.replace(condition_block);
+
+        let to_fix = condition(self).map(|looping| {
+            let next = self.current_block() + 1;
+            self.finish_with(Terminator::Branch {
+                condition: Operand::local(looping),
+                fals: BlockId::PLACEHOLDER,
+                tru: next,
+            })
+        });
+
+        self.begin_scope();
+        iter(self);
+        for expr in body {
+            self.lower(*expr);
+        }
+        self.end_scope();
+
+        self.finish_with(Terminator::Goto(condition_block));
+
+        let after_block = self.current_block();
+        if let Some(to_fix) = to_fix {
+            self.body_mut().blocks[to_fix].terminator.complete(after_block);
+        }
+
+        let breaks = mem::replace(&mut self.current_mut().breaks, prev_loop);
+        self.current_mut().continue_block = prev_continue;
+        for block in breaks {
+            self.body_mut().blocks[block].terminator.complete(after_block);
+        }
+    }
+}
